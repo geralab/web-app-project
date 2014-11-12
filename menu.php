92f386bd17e7cdb3fae076483f590fa4da93b789
applegb <brian.applegate@okstate.edu>
2014-11-11 20:17:36 -0600

    menu file

---
+++ menu.php
@@ -0,0 +1,8 @@
+<?php
+echo '<li><a href = "profile.php">PROFILE</a></li> -
+<li><a href = "statistics.php">GLOBAL STATISTICS</a></li> -
+<li><a href = "trailers.php">TRAILERS</a></li> -
+<li><a href = "downloads.php">DOWNLOADS</a></li> -
+<li><a href = "register.php">REGISTER</a></li> -
+<li><a href = "login.php">LOGIN</a><li>';
+?>
