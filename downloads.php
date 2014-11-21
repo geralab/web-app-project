00d7d8089c4c859c6d0e39f2fc08549720388f5e
geralab <geraldblake@geralds-mbp.osustudent.okstate.edu>
2014-11-21 02:30:29 -0600

    DOWNLOAD PAGE UPD DATABASE UPD

--- downloads.php
+++ downloads.php
@@ -1,48 +1,87 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title> BLAPPO'S  </title>
-		<link rel="stylesheet" type="text/css" href="gstyle.css">
-	</head>
-	<body>
-	<?php
-		session_start();
-        echo '<center><div class = "banner"><h1 class = "title">DOWNLOADS</h1></div></center>';
-		echo '<center><ul id="navbar">';
-			echo'<li><a href = "home.php">HOME</a></li>';
-			echo '<li><a href = "statistics.php">GLOBAL STATISTICS</a></li>';
-			echo '<li><a href = "downloads.php">DOWNLOADS</a></li>';
-			echo '<li><a href = "register.php">REGISTER</a></li>';
-			echo '<li><a href = "login.php">LOGIN</a><li>';
-		echo '</ul></center>';
-		echo '<div class = "normal"></div>';
-        if(array_key_exists('loggedIn', $_SESSION))
+<?php
+    echo '<!DOCTYPE html>';
+    echo '<html>';
+    
+    require 'menu.php';
+    
+    echo '<body>';
+    session_start();
+    if($_SESSION['loggedIn'] != 1)
+    {
+        header('Location:login.php');
+    }
+    echo '<center><div class = "hback"><h1 class = "title">'.$_SESSION['user'].'\'s DOWNLOAD PAGE</h1></div></center>';
+    echo '<div class = "normal"></div>';
+    
+    
+    $fileText = file_get_contents('/home/geralab/pass.txt', FILE_USE_INCLUDE_PATH);
+    $dbPassword = trim($fileText);
+    $dbUser = 'geralab';
+    $dbName = $dbUser;
+    $database = new mysqli("cs.okstate.edu", $dbUser, $dbPassword, $dbName);
+    $userName = $_SESSION['user'];
+    $query = "Select link As DOWNLOADS From Download;";
+    $link = "";
+    
+    if (mysqli_connect_errno())
+    {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+    printTable($query,$database);
+    
+    function printTable($query,$database)
+    {
+        $result = $database->query($query);
+        echo "<center><div>\n";
+        if (!is_object($result))
         {
-            if($_SESSION['loggedIn'] == 1)
-            {
-                echo '<div><p class = "gold">'.$_SESSION['user'].'\'s DOWLOADS</p></div>';
-            }
-            else
-            {
-                echo '<div><p class = "gold">DOWLOADS</p></div>';
-            }
+            
         }
-			
-			$fileText = file_get_contents('/home/geralab/pass.txt', FILE_USE_INCLUDE_PATH);
-			$dbPassword = trim($fileText);
-			$dbUser = 'geralab';
-			$dbName = $dbUser; 
-			$database = new mysqli("cs.okstate.edu", $dbUser, $dbPassword, $dbName);
-			//$query = "Select * From Trailer;";
-			if (mysqli_connect_errno()) 
-			{
-				printf("Connect failed: %s\n", mysqli_connect_error());
-				exit();
-			}
-			
-			
-				
-		
-		?>
-	</body>
+        else
+        {
+            // MAKE HTML TABLE
+            echo '<table border="0" cellPadding="1">', "\n";
+            $row = $result->fetch_array(MYSQLI_ASSOC);
+            if ($row)
+            {
+                $keys = array_keys($row);
+                echo '<tr>';
+                foreach ($keys as $key)
+                {
+                    echo "<th>$key</th>";
+                }
+                echo '</tr>';
+                while ($row)
+                {
+                    echo '<tr>';
+                    foreach ($row as $cell)
+                    {
+                        $link = $cell;
+                        $query2 = "Select name From Download Where link = '$link';";
+                        $gI = getInfo($query2,'name',$database);
+                        echo '<td><a href = "'.$cell . '" target = "_blank">'.$gI.'</a></td>';
+                    }
+                    echo '</tr>';
+                    $row = $result->fetch_array(MYSQLI_ASSOC);
+                }
+            }
+            echo "</table>\n";
+            echo "</div></center>";
+        }
+        
+    }
+    function getInfo($query,$col,$database)
+    {
+        $result = $database->query($query);
+        $info='';
+        if (is_object($result))
+        {
+            $row = $result->fetch_array(MYSQLI_ASSOC);
+            $info = $row[$col];
+        }
+        return $info;
+    }
+    ?>
+</body>
 </html>
