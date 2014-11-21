2e1e6d9a83bc4e3bf3bd250868a2f38b5406a63e
geralab <geraldblake@geralds-mbp.osustudent.okstate.edu>
2014-11-21 05:23:55 -0600

    UNITY GAME SUPPORT ADDED

--- direct.php
+++ direct.php
@@ -51,25 +51,34 @@
 					$gameId = $row['gameId'];
 					$fileName = $row['fileName'];
                     $pic = $row['pic'];
-                    list($width, $height, $type, $attr) = getimagesize("$fileName");
-					//echo '<div class = "contain">';
-					echo "<center><div class = \"hback\"><h1 class = \"title\">$gameId</h1></div>";
-                    echo '<div class = "gameDiv"><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="'.$width.'" height="'.$height.'" id="'.$gameId.'" align="middle">';
-                    echo '<param name="movie" value="'.$fileName.'"/>';
-                    echo '<!--[if !IE]>-->';
-                       echo '<object type="application/x-shockwave-flash" data="'.$fileName.'" width="'.$width.'" height="'.$height.'">';
+                    $type = $row['type'];
+                    echo "<center><div class = \"hback\"><h1 class = \"title\">$gameId</h1></div>";
+                    if($type == "flash")
+                    {
+                        list($width, $height, $type, $attr) = getimagesize("$fileName");
+                        //echo '<div class = "contain">';
+                        
+                        echo '<div class = "gameDiv"><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="'.$width.'" height="'.$height.'" id="'.$gameId.'" align="middle">';
                         echo '<param name="movie" value="'.$fileName.'"/>';
-                        echo '<!--<![endif]-->';
-                        echo '<a href="http://www.adobe.com/go/getflash">';
-                        echo '<img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player"/>';
-                        echo '</a><div class ="descript"><p>Please click the link above to install flash player and play '.$gameId.' or to find out if adobe flash is supported by your device</p></div><div class = "display"><img src = "'.$pic.'"></img></div>';
                         echo '<!--[if !IE]>-->';
-                           echo '</object>';
+                           echo '<object type="application/x-shockwave-flash" data="'.$fileName.'" width="'.$width.'" height="'.$height.'">';
+                            echo '<param name="movie" value="'.$fileName.'"/>';
                             echo '<!--<![endif]-->';
-                            echo '</object></div>';
-					//echo '</div>';
-					echo '<br><br>';
-				}
+                            echo '<a href="http://www.adobe.com/go/getflash">';
+                            echo '<img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player"/>';
+                            echo '</a><div class ="descript"><p>Please click the link above to install flash player and play '.$gameId.' or to find out if adobe flash is supported by your device</p></div><div class = "display"><img src = "'.$pic.'"></img></div>';
+                            echo '<!--[if !IE]>-->';
+                               echo '</object>';
+                                echo '<!--<![endif]-->';
+                                echo '</object></div>';
+                        //echo '</div>';
+                        echo '<br><br>';
+                    }
+                    else if($type == "unity")
+                    {
+                        header('Location:'.$fileName);
+                    }
+                }
 				echo "<br/></center>";
 			} 
 ?>
