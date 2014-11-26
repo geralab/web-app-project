6638c10b0ead90e3cf715c16a4969c82752fd05e
geralab <geraldblake@Geralds-MacBook-Pro.local>
2014-11-26 10:13:21 -0600

    dnddkjgkldsjgk

--- home.php
+++ home.php
@@ -7,6 +7,7 @@ echo '<html>';
 			
 		
 			session_start();
+            $userId = '';
             if(array_key_exists('loggedIn', $_SESSION))
             {
                 if($_SESSION['loggedIn'] == 1)
@@ -18,6 +19,7 @@ echo '<html>';
                 {
                     echo '<center><div class = "hback"><h1 class = "title"> QUAZZAR HOME</h1></div></center>';
                 }
+                $userId = $_SESSION['user'];
             }
 	
 			$fileText = file_get_contents('/home/geralab/pass.txt', FILE_USE_INCLUDE_PATH);
@@ -60,7 +62,15 @@ echo '<html>';
 							echo '<h3 class = "w">'.$gameId.'</h3>';
 							echo '<div class = "sub"><a href = "direct.php?direct='.$gameId.'"><img class ="display" src = "'.$pic.'"></a></div>';
 							echo "<div class = \"descript\"><span><p class = \"center\">$description
-							NUMBER OF LIKES: $likes</p></span><br><input class = \"button\"  name = \"like\" value=\"LIKE\"></div>";
+							NUMBER OF LIKES: $likes</p></span><br>";
+                            if(array_key_exists('loggedIn', $_SESSION))
+                            {
+                                if($_SESSION['loggedIn'] == 1)
+                                {
+                                    echo "<input class = \"button\"  name = \"like\" value=\"LIKE\">";
+                                }
+                            }
+                            echo "</div>";
 							echo "</div></center><br/>";
 							$row = $result->fetch_array(MYSQLI_ASSOC);
                             $count = $count + 1;
@@ -77,7 +87,8 @@ echo '<html>';
 				$query = "Select * From Game Where gameId Like '$search%';";
 				$result = $database->query($query);
                 $count = 0;
-                $divString="";
+                $divString = "";
+                
 				if (!is_object($result))
 				{
 
@@ -106,7 +117,15 @@ echo '<html>';
 							echo '<h3 class = "w">'.$gameId.'</h3>';
 							echo '<div class = "sub"><a href = "direct.php?direct='.$gameId.'"><img class ="display" src = "'.$pic.'"></a></div>';
 							echo "<div class = \"descript\"><span><p class = \"center\">$description
-							NUMBER OF LIKES: $likes</p></span><br><input class = \"button\"  name = \"like\" value=\"LIKE\"></div>";
+							NUMBER OF LIKES: $likes</p></span><br>";
+                            if(array_key_exists('loggedIn', $_SESSION))
+                            {
+                                if($_SESSION['loggedIn'] == 1)
+                                {
+                                    echo "<input class = \"button\"  name = \"like\" value=\"LIKE\">";
+                                }
+                            }
+                            echo "</div>";
 							echo "</div></center><br/>";
 							$row = $result->fetch_array(MYSQLI_ASSOC);
                             $count = $count + 1;
